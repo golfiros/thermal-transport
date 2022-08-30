27aa66a049ceb36aef039e9d7b834d4a02e31775
golfiros <gabriel.golfetti@gmail.com>
2022-08-30 15:26:23 -0300

    added numerical integrator

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ double P_TRANS_PROB[3 * (3 - 1) / 2]; //transmission probabilities
 
 double E_MIN, E_MAX; //integration bounds given by bands
 
-void setup(double* t, double* mu, double* v, double delta){
+void setup(double *t, double *mu, double *v, double delta){
   for(int i=0;i<3;i++){
     P_T[i] = *t;
     P_INV_T[i] = 1 / *(t++);
@@ -28,7 +28,7 @@ void setup(double* t, double* mu, double* v, double delta){
   DELTA = delta;
 }
 
-void set_bias(double* bias){
+void set_bias(double *bias){
   double x, t, min, max;
   for(int i=0;i<3;i++){
     x = P_MU[i] + *(bias++);
@@ -81,7 +81,7 @@ void find_trans(double E){
   }
   double den = 1 / (gf * conj(gf));
   
-  double* out = P_TRANS_PROB;
+  double *out = P_TRANS_PROB;
   for(int i=0;i<3-1;i++){
     for(int j=i+1;j<3;j++){
       *out = 4 * P_V[i] * P_V[i] * P_V[j] * P_V[j] * 
@@ -110,43 +110,52 @@ void test_find_trans(int steps){
   }
 }
 
+double fermi_dirac(double beta, double E){
+  return 1 / (exp(beta * E) + 1);
+}
+
+//numerical integration by adaptive quadrature
+//accurate within |b - a| * tol
+#define MAX_REC 512 //max subdivisions for integration
+double INTEGRAL_STACK[2 * MAX_REC];
+double integrate(double (*func)(double), double a, double b, double tol){
+  double integral = 0.0;
+  double i1, i2, m;
+  INTEGRAL_STACK[0] = a;
+  INTEGRAL_STACK[1] = b;
+  int sp = 1;
+  while(sp > 0){
+    b = INTEGRAL_STACK[2 * --sp + 1];
+    a = INTEGRAL_STACK[2 * sp];
+    i1 = 0.5 * (b - a) * ((*func)(a) + (*func)(b));
+    m = 0.5 * (a + b);
+    i2 = 0.25 * (b - a) * ((*func)(a) + 2 * (*func)(m) + (*func)(b));
+    if(fabs(i1-i2) < 3 * fabs(b - a) * tol || sp > MAX_REC - 2){ integral += i2; continue; }
+    INTEGRAL_STACK[2 * sp] = a;
+    INTEGRAL_STACK[2 * sp++ + 1] = m;
+    INTEGRAL_STACK[2 * sp] = m;
+    INTEGRAL_STACK[2 * sp++ + 1] = b;
+  }
+  return integral;
+}
+
 int main(int argc, char** argv){
   //this program evaluates the current between two probes 
   //coupled to an impurity site, which is then coupled
   //to a third probe that functions as a bath, making the
   //process inelastic in nature.
   
+  /*
   double t[3] = {1.0,1.0,1.0}; 
   double mu[3] = {0.0,0.0,0.0};
   double v[3] = {0.2,0.3,0.1};
   double delta = 0.3;
-
   setup(t, mu, v, delta);
 
   double bias[3] = {0.5,-0.5,0.1};
-
   set_bias(bias);
-
-  //test_find_momenta(20);
-  //test_find_trans(50);
-
-  //TEST OF compute_transmissions
-  /*
-  #define N_CONDUCTORS 4
-  double energy;
-  double E_min = -4.0;
-  double E_max = 4.0;
-  int steps = 20;
-  for(int i=0;i<steps;i++){
-    energy = E_min + (E_max - E_min) * ((double)i+0.5) / steps;
-    printf("Energy: %f, Transmissions: ", energy);
-    compute_transmissions(transmission, N_CONDUCTORS, mu, inv_t, v, delta, energy);
-    for(int j=0;j<N_CONDUCTORS*(N_CONDUCTORS-1)/2;j++){
-      printf("%f ", transmission[j]);
-    }
-    printf("\n");
-  }
-  free(P_MOMENTA);
+  test_find_momenta(20);
+  test_find_trans(50);
   */
 
   return 0;
